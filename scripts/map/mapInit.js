79d700b2e797c1944f99080fb36b946f08670f0f
Aeeluo <aeeluo.ytb@gmail.com>
2025-05-13 23:31:31 +0200

    Changed the layers object to seperate the URLs from the tileLayer function.

--- scripts/map/mapInit.js
+++ scripts/map/mapInit.js
@@ -5,31 +5,40 @@ const map = L.map('map', {
 
 })
 
+const apiURLs = {
+    OpenStreetMap: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    OpenTopoMap: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    CartoDBPositron: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    CartoDBDarkMatter: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+    ESRIWorldImagery: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    Terrarium: "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png",
+}
+
 const layers = {
-    OpenStreetMap: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    OpenStreetMap: L.tileLayer(apiURLs['OpenStreetMap'], {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19,
     }),
-    OpenTopoMap: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    OpenTopoMap: L.tileLayer(apiURLs['OpenTopoMap'], {
         attribution: '© OpenTopoMap (CC-BY-SA)',
         maxZoom: 17,
     }),
-    CartoDBPositron: L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    CartoDBPositron: L.tileLayer(apiURLs['CartoDBPositron'], {
         attribution: '&copy; <a href="https://www.carto.com/">CartoDB</a>',
         subdomains: "abcd",
         minZoom: 0,
         maxZoom: 19,
     }),
-    CartoDBDarkMatter: L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    CartoDBDarkMatter: L.tileLayer(apiURLs['CartoDBDarkMatter'], {
         attribution: '&copy; <a href="https://www.carto.com/">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 20,
     }),
-    ESRIWorldImagery:  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    ESRIWorldImagery:  L.tileLayer(apiURLs['ESRIWorldImagery'], {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community',
         maxZoom: 18,
     }),
-    terrariumLayer: L.tileLayer('https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png', {
+    terrariumLayer: L.tileLayer(apiURLs['Terrarium'], {
         tileSize: 256,
         opacity: 0.3,
         attribution: 'Mapzen terrain tiles via AWS',
@@ -45,8 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         map.eachLayer((layer) => map.removeLayer(layer));
         layers[selectedDataset].addTo(map);
-        console.log(`Switched to dataset: ${selectedDataset}`);
     });
 });
    
-export { map };
+export { map, apiURLs };
